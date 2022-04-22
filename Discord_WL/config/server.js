e3c4d972f8ad168159ec861528e7df5402708093
08rafa <104206689+08rafa@users.noreply.github.com>
2022-04-22 11:01:42 -0300

    Update server.js
    O codigo foi comentado de maneira mais detalhada.

--- Discord_WL/config/server.js
+++ Discord_WL/config/server.js
@@ -1,15 +1,15 @@
 module.exports = {
     canal_wl: "959502345211875409", // Canal principal.
-    categoria_wl: "959502306422980639",
-    logs_staff: "959502431765528627",
-    resultados: "959502379806507098",
-    canal_regras: "966846068069388328",
-    call_ou_canal_suporte: "966846137187323954",
+    categoria_wl: "959502306422980639", // Categoria onde os canais serao criados.
+    logs_staff: "959502431765528627", // Canal logs staff.
+    resultados: "959502379806507098", // Canal resultados.
+    canal_regras: "966846068069388328", // Canal regras.
+    call_ou_canal_suporte: "966846137187323954", // Canal ou call de suporte.
     // [CARGOS]
     sem_wl: "959502568281739274", // Cargo usado no autorole + cargo que sera retirado apos ser aprovado.
     com_wl: "959502515848757278", // Cargo dado aos membros que passarem.
     // [OUTROS]
     cor_das_embeds: "PURPLE", // Cor das embeds principais.
-    logo: "https://media.discordapp.net/attachments/927003360907915264/927003388879724624/standard.gif?width=192&height=192",
-    footer: "Developed by rafa",
+    logo: "https://media.discordapp.net/attachments/927003360907915264/927003388879724624/standard.gif?width=192&height=192", // Logo do seu servidor.
+    footer: "Developed by rafa", // Footer da maioria das embeds.
 }
